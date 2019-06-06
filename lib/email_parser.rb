edafdd72215d23aaa5d873203231f345f5e5b980
Tom White <eugene.white@gmail.com>
2019-06-06 00:19:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,6 +14,7 @@ class EmailParser
   
   def parse 
     @emailstring.split(/[\s,]/).reject{|c| c.empty?}.uniq
+    #splits the string at commas and whitespace, rejecting empty array elements, and only returning unique array elements
   end
   
 end
